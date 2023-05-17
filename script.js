819d5b957cdc8f27ba362270bd020387952e5a9e
tobjiMoh <mohamed.tobji@lycee-jeanrostand.fr>
2023-05-17 09:57:48 +0200

    projet initial
    projet initial.
    index.html et style.css sont complets.
    Seul le fichier script.js est à coder.

---
+++ script.js
@@ -0,0 +1,10 @@
+// recuperer toutes les balises de classe 'panel'
+
+
+// pour chaque DIV de la collection d'objets de classe 'panel'
+    // ajouter un ecouteur d'evenement 'clic'
+    // la fonction de callback associée doit :
+        // enlever la classe active de toutes les DIV
+        // ajouter la classe active pour la DIV ayant déclenchée l'évenement 'clic'
+
+
